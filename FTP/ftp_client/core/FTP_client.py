f04ba57474001df177a3d0527cd10c2a0aa2df11
shaojie <1115041402@qq.com>
2017-12-01 15:29:09 +0800

    FTP小程序

---
+++ FTP/ftp_client/core/FTP_client.py
@@ -0,0 +1,117 @@
+# _author_='shaojie'
+# -*- coding:utf-8 -*-
+
+import socket
+import os
+import json
+
+class FTP(object):
+
+    def __init__(self):
+        self.client = socket.socket()
+
+    def connect(self, host, port):
+        self.client.connect((host, port))
+
+    def help(self):
+        menu = u'''
+        ls
+        cd
+        pwd
+        get filename
+        push filename
+        '''
+        print(menu)
+
+    def cd(self, *args):
+        cmd_str = args[0]
+        data = {'action':'cd',
+                'filename':cmd_str.split()[1]}
+        self.client.send(json.dumps(data).encode())
+        response = self.client.recv(1024).decode()
+        print(response)
+
+    def ls(self,args):
+        cmd = args[0]
+        # print(cmd)
+        data = {
+                    'action': 'ls'
+                }
+        self.client.send(json.dumps(data).encode())
+        response = self.client.recv(1024)
+        print(response)
+
+
+    def pwd(self,*args):
+        cmd = args[0]
+        # print(cmd)
+        data = {
+            'action':'pwd'
+        }
+        self.client.send(json.dumps(data).encode())
+        response = self.client.recv(1024)
+        print(response)
+
+    def put(self, *args):
+        cmd_dic = args[0].split()
+        print(cmd_dic)
+        if len(cmd_dic) > 1:
+            filename = cmd_dic[1]
+            if os.path.isfile(filename):
+                filesize = os.stat(filename).st_size
+                data = {
+                    'filename': filename,
+                    'size': filesize,
+                    'action': 'put'
+                }
+                self.client.send(json.dumps(data).encode())  # json序列化
+                code = self.client.recv(1024)  # 防止粘包
+                print(code)
+                f = open(filename, 'rb')
+                for line in f:
+                    self.client.send(line)
+                else:
+                    print('文件发送完毕')
+                    f.close()
+            else:
+                print('filename not exist')
+        else:
+            self.help()
+
+    def get(self, *args):
+        cmd_str = args[0]
+        filename = cmd_str.split()[1]
+        data = {
+            'action': 'get',
+            'filename': filename
+        }
+        self.client.send(json.dumps(data).encode())
+        filesize = self.client.recv(1024).decode()
+        self.client.send(b'200')  # ACK应答,防止粘包
+        size = 0
+        f = open(filename,'wb')
+        while size < int(filesize):
+            msg = self.client.send(1024)
+            f.write(msg)
+            size += len(msg)
+        else:
+            f.close()
+            print('receive file success')
+
+    def interaction(self):
+        while True:
+            cmd_str = input('>>:')
+            if len(cmd_str) == 0:continue
+            cmd = cmd_str.split()[0]
+            print(cmd)
+            if hasattr(self, cmd):
+                fun = getattr(self, cmd)
+                # print(fun)
+                fun(cmd_str)
+            else:
+                print('input command error,re-enter')
+                self.help()
+
+f = FTP()
+f.connect('112.74.35.102',9999)
+f.interaction()
---
+++ FTP/ftp_server/core/FTP_server.py
@@ -0,0 +1,94 @@
+# _author_='shaojie'
+# -*- coding:utf-8 -*-
+
+import socketserver
+import json
+import os
+
+
+class Me_socket(socketserver.BaseRequestHandler):
+
+    def setup(self):
+        self.current_dir = os.path.dirname(os.getcwd()) + '/home'
+
+    def cd(self,*args):
+        cmd_dic = args[0]
+        print(cmd_dic)
+        file = cmd_dic['filename']
+        if file == '..':
+            self.current_dir = os.path.dirname(self.current_dir)
+            self.request.send(self.current_dir.encode())
+        elif os.path.isdir(self.current_dir + '/' + file):
+            self.current_dir = self.current_dir + '/' + file
+            print(self.current_dir)
+            self.request.send(self.current_dir.encode())
+        else:
+            print('cmd input error,again input ')
+            self.request.send(b'400,error')
+
+    def pwd(self, *args):
+        self.request.send(self.current_dir.encode())
+
+    def ls(self,*args):
+        file = os.listdir(self.current_dir)
+        print(file)
+        self.request.send(str(file).encode())
+
+    def put(self, *args):
+        cmd_dic = args[0]
+        filename = cmd_dic['filename']
+        filesize = cmd_dic['size']
+        if os.path.isfile(filename):
+            f = open(filename + 'new', 'wb')
+        else:
+            f = open(filename, 'wb')
+        self.request.send(b'200,ok')
+        size = 0
+        while size < filesize:
+            data = self.request.recv(1024)
+            f.write(data)
+            size += len(data)
+            # print('已成功接收文件百分之%s'% int(filesize/size))
+        else:
+            f.close()
+            print('receive file success')
+
+    def get(self, *args):
+        cmd_dic = args[0].split()
+        # print(cmd_dic)
+        if len(cmd_dic) > 1:
+            filename = cmd_dic[1]
+            file = self.current_dir + '/' + filename  # 拼接到HOME目录,只允许用户访问自己所在的目录
+            if os.path.isfile(file):
+                self.request.send(str(os.stat(file).st_size).encode())  # 发送文件大小
+                self.request.recv(1024)  # ACK应答
+                f = open(file, 'rb')
+                for line in f:
+                    self.request.send(line)
+                else:
+                    f.close()
+                    print('send file success')
+            else:
+                self.request.send(b'403,error')
+
+    def handle(self):
+        while True:
+            try:
+                self.msg = self.request.recv(1024)
+                print('%s:%s execute %s'%(self.client_address[0],self.client_address[1],self.msg.decode()))
+                cmd_dic = json.loads(self.msg.decode())
+                action = cmd_dic['action']
+                if hasattr(self, action):
+                    fun = getattr(self, action)
+                    fun(cmd_dic)
+            except ConnectionResetError as e:
+                print(e)
+                break
+
+
+if __name__ == "__main__":
+
+    host, port = 'localhost',9999
+    s = socketserver.ThreadingTCPServer((host,port),Me_socket)
+    s.serve_forever()
+
